f40005e6c395c158f4a414947d7d6559d1343d5c
Andrew Brown <andrew@monsterboxpro.com>
2022-11-20 22:08:50 +0000

    fix

--- frontend/src/components/ActivityForm.js
+++ frontend/src/components/ActivityForm.js
@@ -4,17 +4,33 @@ import React from "react";
 export default function ActivityForm() {
   const [count, setCount] = React.useState(0);
 
+  const state = {value: ''};
+
   const classes = []
   classes.push('count')
   if (240-count < 0){
     classes.push('err')
   }
 
+  const onsubmit = (event) => {
+    console.log('submitting',state);
+    event.preventDefault();
+  }
+
+  const textarea_onchange = (event) => {
+    setCount(event.target.value.length)
+    React.setState({value: event.target.value})
+  }
+
   return (
-    <form className='activity_form'>
+    <form 
+      className='activity_form'
+      onSubmit={onsubmit}
+    >
       <textarea
+        value={state.value}
         placeholder="what would you like to say?"
-        onChange={e => setCount(e.target.value.length)}
+        onChange={textarea_onchange} 
       />
       <div className='submit'>
         <div className={classes.join(' ')}>{240-count}</div>
