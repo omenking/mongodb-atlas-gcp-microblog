2b1ed2884750f5864d7003e82bb8fac8e6ccf1b8
Andrew Brown <andrew@monsterboxpro.com>
2022-12-08 20:16:31 +0000

    test

--- frontend/src/pages/UserFeedPage.js
+++ frontend/src/pages/UserFeedPage.js
@@ -2,13 +2,36 @@ import './UserFeedPage.css';
 import ActivityFeed from '../components/ActivityFeed';
 import { useParams } from 'react-router-dom';
 
-export default function UserFeedPage(props) {
+export default function UserFeedPage() {
+  const [activities, setActivities] = React.useState([]);
+
   const params = useParams();
   const title = `@${params.handle}`;
 
+  const loadData = async () => {
+    try {
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${title}`
+      const res = await fetch(backend_url, {
+        method: "GET"
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setActivities(resJson)
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  React.useEffect(()=>{
+    loadData();
+  }, [])
+
   return (
     <article>
-      <ActivityFeed title={title} activities={props.activities} />
+      <ActivityFeed title={title} activities={activities} />
     </article>
   );
 }
